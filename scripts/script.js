a20e955fa838000544b3fb7f56ea9d9629b2af4b
mucyoernest <43173220+mucyoernest@users.noreply.github.com>
2021-10-18 00:42:12 +0200

    init

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+function openForm() {
+    document.getElementById("loginForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("loginForm").style.display = "none";
+  }
+
+  function openSignForm() {
+    document.getElementById("signUpForm").style.display = "block";
+  }
+  
+  function closeSignForm() {
+    document.getElementById("signUpForm").style.display = "none";
+  }
